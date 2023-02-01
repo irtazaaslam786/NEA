f685a41116cec726b0d20867f9d23337a57fe6e8
AhmetKucukAK <54936799+AhmetKucukAK@users.noreply.github.com>
2023-02-01 17:38:53 +0000

    Add files via upload

--- DatabaseController.py
+++ DatabaseController.py
@@ -1,42 +1,57 @@
 import sqlite3
 
+CHECK_IF_EMPLOYEES_EXISTS = "SELECT name FROM sqlite_master WHERE type='table' AND name='Employees'"
+CHECK_IF_SHIFTS_EXISTS = "SELECT name FROM sqlite_master WHERE type='table' AND name='Shifts'"
+
 class DatabaseController():
     
     def __init__(self, filePath):
         try:
             self.connection = sqlite3.connect(filePath)
-            self.cursor = self.connection.cursor()
             print("Database initialised")
+            cursor = self.connection.cursor()
+            
+            cursor.execute(CHECK_IF_EMPLOYEES_EXISTS)
+            if cursor.fetchall() == []:  #Employees table does not exist
+                self.createEmployeesTable()
+                print("Employees table successfully created")
+
+            cursor.execute(CHECK_IF_SHIFTS_EXISTS)
+            if cursor.fetchall() == []:  #Shifts table does not exist
+                self.createShiftsTable()
+                print("Shifts table successfully created")
+
+            cursor.close()
         except sqlite3.Error as error:
             print("Error initialising database")
             print(error)
             
     def executeSelectQuery(self, query):
-        self.cursor = self.connection.cursor()
-        results = self.cursor.execute(query)
+        cursor = self.connection.cursor()
+        results = cursor.execute(query)
         resultSet = []
         for row in results:
             resultSet.append(row)
             #each row is a tuple
-        self.cursor.close()
+        cursor.close()
         return resultSet
         
     def executeQuery(self, query):
         try:
-            self.cursor = self.connection.cursor()
-            self.cursor.execute(query)
+            cursor = self.connection.cursor()
+            cursor.execute(query)
             self.connection.commit()
         except sqlite3.Error as error:
             print(error)
             self.closeDatabase()
         finally:
-            self.cursor.close()
+            cursor.close()
 
     def closeDatabase(self):
         self.connection.close()
         print("Database closed")
 
-    def createEmployeeTable(self):
+    def createEmployeesTable(self):
         try:
             self.connection.execute("""CREATE TABLE Employees(
                                     EmployeeID integer primary key, 
