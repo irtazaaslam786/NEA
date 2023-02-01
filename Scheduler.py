ab7672c87e932d2645d1f40c7b85d3f6d6afcfa4
irtazaaslam786 <123005558+irtazaaslam786@users.noreply.github.com>
2023-02-01 17:42:39 +0000

    Add files via upload

--- Scheduler.py
+++ Scheduler.py
@@ -3,7 +3,7 @@ from DatabaseController import *
 class Scheduler:
     def __init__(self, filename):
         self.DBcontroller = DatabaseController(filename)
-        
+
     def getShiftsForDay(self, date):
         query = """SELECT Employees.firstName, Employees.surname, Shifts.startTime, Shifts.endTime, Shifts.breakTime 
         FROM Shifts JOIN Employees 
@@ -16,6 +16,11 @@ class Scheduler:
 
     def addShift(self, employeeID, startTime, endTime, date, breakTime):
         self.DBcontroller.createShift(employeeID, startTime, endTime, date, breakTime)
-    
+
     def closeDatabase(self):
         self.DBcontroller.closeDatabase()
+
+    def getEmployeeData(self, employeeID, firstName, surname, age):
+        self.DBcontroller.getEmployeeData(employeeID, firstName, surname, age)
+
+  
