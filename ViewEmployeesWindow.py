44282ff40779863d48dbf52038d075069fbe4ceb
AhmetKucukAK <54936799+AhmetKucukAK@users.noreply.github.com>
2023-02-15 17:27:48 +0000

    Add files via upload

---
+++ ViewEmployeesWindow.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import tksheet
+
+from Window import *
+
+class ViewEmployeesWindow(Window):
+    def __init__(self, master, geometry, scheduler):
+        super().__init__(master, geometry, scheduler)
+        self.window.title("View Employees")
+
+        self.createEmployeesTable()
+        self.table.pack()
+
+    def createEmployeesTable(self):
+        self.table = tksheet.Sheet(self.window, width = 300, height = 200)  #make dimensions dynamic
+        shiftData, columns = self.scheduler.getEmployeeData()
+        self.table.headers = columns
+        self.table.set_sheet_data(shiftData)
