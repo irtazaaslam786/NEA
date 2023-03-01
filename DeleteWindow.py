9e2a93e4db3575219fb70a6ca7971c1172f012a3
AhmetKucukAK <54936799+AhmetKucukAK@users.noreply.github.com>
2023-03-01 17:41:40 +0000

    Add files via upload

--- DeleteWindow.py
+++ DeleteWindow.py
@@ -5,3 +5,31 @@ class DeleteWindow(Window):
     def __init__(self, master, geometry, scheduler):
         super().__init__(master, geometry, scheduler)
         self.window.title("Delete Employees")
+
+        self.createDeleteEmployeeButton()
+        self.createDeleteShiftButton()
+        self.createDeleteHolidayButton()
+
+    def createDeleteEmployeeButton(self):
+        self.employeeButton = tk.Button(self.window, text="Delete Employee", command=self.createDeleteEmployeeWindow)
+        self.employeeButton.pack()
+
+    def createDeleteShiftButton(self):
+        self.shiftButton = tk.Button(self.window, text="Delete Shift", command=self.createDeleteShiftWindow)
+        self.shiftButton.pack()
+
+    def createDeleteHolidayButton(self):
+        self.holidayButton = tk.Button(self.window, text="Delete Holiday", command=self.createDeleteHolidayWindow)
+        self.holidayButton.pack()
+
+    def createDeleteEmployeeWindow(self):
+        #self.DeleteEmployeeWindow = AddEmployeeWindow(self.window, "500x300", self.scheduler)
+        pass
+
+    def createDeleteShiftWindow(self):
+        #self.DeleteShiftWindow = DeleteShiftWindow(self.window, "500x300", self.scheduler)
+        pass
+
+    def createDeleteHolidayWindow(self):
+        #self.DeleteHolidayWindow = DeleteHolidayWindow(self.window, "500x300", self.scheduler)
+        pass
